66e9d78ddde2bafbb7dadac39fd014cffb2aff53
Syed <200816@cognizant.com>
2019-04-18 19:31:29 +0530

    Initial version

---
+++ src/test/java/com/cognizant/outreach/OutreachUiProxyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cognizant.outreach;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OutreachUiProxyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
